99b5c79412fd257dc4549d8aa2fd0382faf92e40
Maxime Langlois <maxime.lngls21@gmail.com>
2024-05-08 13:04:21 +0200

    add filter on image in server side :white_check_mark:

--- camagru/src/save_image.php
+++ camagru/src/save_image.php
@@ -2,22 +2,40 @@
 include 'controllers/database.php';
 session_start();
 
-if (isset($_POST['image'])) {
-    $data = $_POST['image'];
-    $data = str_replace('data:image/png;base64,', '', $data);
-    $data = str_replace(' ', '+', $data);
-    $imageData = base64_decode($data);
+if (isset($_POST['image']) && isset($_POST['filtered_image'])) {
+    $data_fond = $_POST['image'];
+    $data_fond = str_replace('data:image/png;base64,', '', $data_fond);
+    $data_fond = str_replace(' ', '+', $data_fond);
+    $imageDataFond = base64_decode($data_fond);
     
+    $data_superposee = $_POST['filtered_image'];
+    $data_superposee = str_replace('data:image/png;base64,', '', $data_superposee);
+    $data_superposee = str_replace(' ', '+', $data_superposee);
+    $imageDataSuperposee = base64_decode($data_superposee);
+    
+    // Charger l'image de fond
+    $image_fond = imagecreatefromstring($imageDataFond);
+    
+    // Charger l'image à superposer
+    $image_superposee = imagecreatefromstring($imageDataSuperposee);
+    
+    // Superposer l'image à superposer sur l'image de fond
+    $x = 0; // Position horizontale de l'image superposée sur l'image de fond
+    $y = 0; // Position verticale de l'image superposée sur l'image de fond
+    imagecopy($image_fond, $image_superposee, $x, $y, 0, 0, imagesx($image_superposee), imagesy($image_superposee));
+
     // Nom de fichier unique
-    $fileName = 'webcam_image_' . uniqid() . '.png';
+    $fileName = 'superposed_image_' . uniqid() . '.png';
     
-    // Enregistrement de l'image sur le serveur
-    file_put_contents('uploads/' . $fileName, $imageData);
-    
-    echo $fileName;
+    // Enregistrement de l'image superposée sur le serveur
+    imagepng($image_fond, 'uploads/' . $fileName);
 
+    // Libérer la mémoire
+    imagedestroy($image_fond);
+    imagedestroy($image_superposee);
+
+    // Insertion dans la base de données
     $user_id = $_SESSION['user_id'];
-
     $requete = $connexion->prepare("INSERT INTO gallery VALUES (0, :img, :user_id)");
     $requete->execute(
         array(
@@ -26,7 +44,7 @@ if (isset($_POST['image'])) {
         )
     );
 
+    echo $fileName; // Envoyer le nom du fichier superposé au client
 } else {
     echo 'No image data received';
 }
-?>
--- camagru/src/views/montage.php
+++ camagru/src/views/montage.php
@@ -4,23 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../styles/montage3.css">
+    <link rel="stylesheet" href="src/styles/montage.css">
 </head>
 <body>
     <?php include 'header.php'; ?>
     <main>
-        <video id="videoElement" autoplay></video>
-        <button id="captureButton">Capture Photo</button>
-        <canvas id="canvas" style="display: none;"></canvas>
-        <img id="capturedImage" style="display: none;">
-        <form method="POST" name="form1">
-            <input name="hidden_data" id='hidden_data' type="hidden"/>
-        </form>
+        <div class="cameraCol">
+            <video id="videoElement" autoplay></video>
+            <button id="captureButton" class="captureButton">Capture Photo</button>
+            <canvas id="canvas" style="display: none;"></canvas>
+            <img id="filterImage" class="filterImage">
+            <form method="POST" name="form1">
+                <input name="hidden_data" id='hidden_data' type="hidden"/>
+            </form>
+        </div>
+        <div class="filterCol">
+            <button id="filterButton" class="filterButton">Filtre 1</button>
+            <button id="filterButton2" class="filterButton">Filtre 2</button>
+            <button id="filterButton3" class="filterButton">Filtre 3</button>
+            <button id="filterButton4" class="filterButton">Filtre 4</button>
+            <button id="filterButton5" class="filterButton">Filtre 5</button>
+            <button id="filterButton6" class="filterButton">Filtre 6</button>
+            <button id="filterButton7" class="filterButton">Filtre 7</button>
+            <button id="filterButton8" class="filterButton">Filtre 8</button>
+            <button id="filterButton9" class="filterButton">Filtre 9</button>
+            <button id="filterButton10" class="filterButton">Sans Filtre</button>
+            
+        </div>
     </main>
 </body>
 
 
 <script>
+    let SelectFilter = 0;
     document.addEventListener("DOMContentLoaded", function() {
     // Récupérer l'élément vidéo
     var video = document.getElementById("videoElement");
@@ -45,20 +61,93 @@
         var video = document.getElementById('videoElement');
         var canvas = document.getElementById('canvas');
         var context = canvas.getContext('2d');
-        context.drawImage(video, 0, 0, 640, 480);
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
         var imageData = canvas.toDataURL('image/png');
 
-        // Envoi de l'image au serveur
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'src/save_image.php', true);
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xhr.onload = function() {
-            console.log('Image saved:', xhr.responseText);
+        var filterImage = new Image();
+        filterImage.src = 'filtre1.png';
+
+        filterImage.onload = function() {
+        // Dessiner l'image du filtre sur le canevas
+            context.drawImage(filterImage, 0, 0, canvas.width, canvas.height);
+
+            // Récupérer les données de l'image avec le filtre
+            var filteredImageData = canvas.toDataURL('image/png');
+
+            // Envoi des données au serveur
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'src/save_image.php', true);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.onload = function() {
+                console.log('Images saved:', xhr.responseText);
+            };
+            xhr.send('image=' + encodeURIComponent(imageData) + '&filtered_image=' + encodeURIComponent(filteredImageData));
         };
-        xhr.send('image=' + encodeURIComponent(imageData));
     });
+
+
+
+
+    document.getElementById('filterButton').addEventListener('click', function() {
+            var img = new Image();
+            img.onload = function() {
+                document.getElementById('filterImage').src = img.src;
+                document.getElementById('filterImage').style.display = 'block';
+            };
+            img.src = 'filtre1.png';
+            SelectFilter = 1;
+        });
+
+    document.getElementById('filterButton2').addEventListener('click', function() {
+        var img = new Image();
+        img.onload = function() {
+            document.getElementById('filterImage').src = img.src;
+            document.getElementById('filterImage').style.display = 'block';
+        };
+        img.src = 'filtre2.png';
+        SelectFilter = 2;
+    });
+
+    document.getElementById('filterButton10').addEventListener('click', function() {
+        var img = new Image();
+        img.onload = function() {
+            document.getElementById('filterImage').src = img.src;
+            document.getElementById('filterImage').style.display = 'none';
+        };
+        img.src = 'filtre1.png';
+        SelectFilter = 0;
+    });
+
+
+
 });
 
 
 </script>
 </html>
+
+captureButton.addEventListener("click", function() {
+        // Dessiner la vidéo de la caméra sur le canvas
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        // Dessiner une image superposée
+        var image = new Image();
+        image.src = 'chemin/vers/votre/image.png'; // Chemin de votre image à superposer
+        image.onload = function() {
+            // Dessiner l'image superposée sur le canvas
+            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+            
+            // Convertir le contenu du canvas en image
+            var imageData = canvas.toDataURL('image/png');
+
+            // Envoi de l'image au serveur
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'src/save_image.php', true);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.onload = function() {
+                console.log('Image saved:', xhr.responseText);
+            };
+            xhr.send('image=' + encodeURIComponent(imageData));
+        };
+    });
