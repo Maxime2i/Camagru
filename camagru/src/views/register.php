442409faefda0ee2089785d543862d18aa4410d3
Maxime Langlois <maxime.lngls21@gmail.com>
2024-05-24 19:42:24 +0200

    add style in register page :lipstick:

--- camagru/src/views/register.php
+++ camagru/src/views/register.php
@@ -4,21 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="src/styles/login.css">
+    <link rel="stylesheet" href="src/styles/register.css">
 </head>
 <body>
-<form action="?page=register&action=submit" method="post" onsubmit="return validateForm()">
+    <main>
 
-        <h2>Register</h2>
-        <input type="text" name="firstname" placeholder="Firstname" required>
-        <input type="text" name="lastname" placeholder="Lastname" required>
-        <input type="text" name="username" id="username" placeholder="Username" required>
-        <span id="username-error" style="color: red;"></span>
-        <input type="email" name="email" placeholder="E-mail" required>
-        <input type="password" name="password" id="password" placeholder="Password" required>
-        <span id="password-error" style="color: red;"></span>
-        <input type="submit" value="Register" name="submit">
-    </form>
+        <form action="?page=register&action=submit" method="post" onsubmit="return validateForm()">
+            <h2>Register</h2>
+            <input type="text" name="firstname" placeholder="Firstname" required>
+            <input type="text" name="lastname" placeholder="Lastname" required>
+            <input type="text" name="username" id="username" placeholder="Username" required>
+            <span id="username-error" style="color: red;"></span>
+            <input type="email" name="email" placeholder="E-mail" required>
+            <input type="password" name="password" id="password" placeholder="Password" required>
+            <span id="password-error" style="color: red;"></span>
+            <input type="submit" value="Register" name="submit">
+        </form>
+    </main>
+
 </body>
 
 <script>
